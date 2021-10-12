a9be37275e01741e5f85f5c0278548c7bf79f650
Catherine <catherinedu2000@gmail.com>
2021-10-12 17:13:06 -0400

    spotify ios sdk authorization

---
+++ SpotiFriends-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SpotiFriends-Bridging-Header.h
+//  SpotiFriends
+//
+//  Created by Catherine D on 10/12/21.
+//
+
+//#ifndef SpotiFriends_Bridging_Header_h
+//#define SpotiFriends_Bridging_Header_h
+//
+//
+//#endif /* SpotiFriends_Bridging_Header_h */
+
+#import <SpotifyiOS/SpotifyiOS.h>
---
+++ SpotiFriends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotiFriends
+//
+//  Created by Catherine D on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Spotifriends")
+            .padding()
+        Spacer()
+        Text("Click to login")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

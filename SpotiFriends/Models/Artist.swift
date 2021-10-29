c7b4ac1b399f06849a64f978426443166f02318f
Xinyi Huang <xinyih@weh5201-a5.andrew.cmu.edu>
2021-10-29 13:41:59 -0400

    Added Artist Model

--- SpotiFriends/Models/Artist.swift
+++ SpotiFriends/Models/Artist.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseDatabase
+
+class Artist: Identifiable {
+    
+    let ref: DatabaseReference?
+    let id: String
+    let name: String
+    let artist_image_url: String
+    let artist_image: Data?
+    
+    init(id: String, name: String, artist_image_url: String) {
+        self.ref = nil
+        self.id = id
+        self.name = name
+        self.artist_image_url = artist_image_url
+        
+        let url = URL(string: self.artist_image_url!)
+        let artist_image = try? Data(contentsOf:url!)
+    }
+    
+    init?(snapshot: DataSnapshot) {
+       // This is what we call a failable init because
+       // it returns nil if initialization fails
+       guard
+        let value = snapshot.value as? [String: AnyObject],
+        let id = value["id"] as? String,
+        let song_name = value["name"] as? String,
+        let artist_image_url = value["artist_image"] as? String
+       else {
+         return nil
+       }
+       
+        self.ref = snapshot.ref
+        self.id = id
+        self.name = song_name
+        self.artist_image_url = artist_image_url
+        let url = URL(string: self.artist_image_url!)
+        let artist_image = try? Data(contentsOf:url!)
+     }
+     
+     func toAnyObject() -> Any {
+       return [
+         "id": id,
+         "name": name,
+         "artist_image_url": artist_image_url,
+       ]
+     }
+}

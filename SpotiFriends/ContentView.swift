e8ebf1dcb62f2b4916498c431469caf0b17e5a22
Joyce Wu <thejoycewu@gmail.com>
2021-11-21 15:33:01 -0500

    make login page look like hi-fi

--- SpotiFriends/ContentView.swift
+++ SpotiFriends/ContentView.swift
@@ -30,6 +30,10 @@ struct ContentView: View {
                     }
                    
                 } else {
+                  VStack {
+                    Text("Spotifriends").foregroundColor(.white).font(.largeTitle).fontWeight(.black)
+                    Image(systemName: "wifi").padding().background(Color.green).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).foregroundColor(.white).font(.largeTitle).frame(width:200, height: 200).scaleEffect(2.0)
+                    Text("the app for meeting people with similar music tastes").frame(width: 240, height: 130, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).multilineTextAlignment(.center).foregroundColor(.gray).font(.title3)
                     Button(action: {
                         vm.getMatches()
                         DispatchQueue.main.asyncAfter(deadline: .now()+10) {
@@ -42,15 +46,22 @@ struct ContentView: View {
                         }
                         
                     }) {
-                        Text("Log In")
-                    }
+                      Text("Log In").foregroundColor(.black).font(.title2).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }.frame(minWidth: 0, maxWidth: 200)
+                    .font(.system(size: 18))
+                    .padding()
+//                    .foregroundColor(.blue)
+                    .background(Color.white)
+                    .cornerRadius(25.0)
+//                    .font(.title)
+//                    .overlay(
+//                        RoundedRectangle(cornerRadius: 25)
+//                            .stroke(Color.white, lineWidth: 2))
+                  } //end of vstack
                 }
             }
                 
-        }
-    
- 
-
+        }.background(Color.black)
     }
 }
 
